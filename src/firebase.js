ec099733792ced7063bee1d6fb64013f61e1d36b
Sahil <sahilkumarm226@gmail.com>
2025-08-05 06:11:53 +0500

    UNEXPOSED CREDENTIALS

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBWZh_hHTDBqgAvPzWxmNBTqiUj3eqMl2o",
-  authDomain: "two-seas-llp.firebaseapp.com",
-  projectId: "two-seas-llp",
-  storageBucket: "two-seas-llp.firebasestorage.app",
-  messagingSenderId: "762862325873",
-  appId: "1:762862325873:web:c1c408818257ee52728bd0",
-  measurementId: "G-WC32TCG3QJ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
